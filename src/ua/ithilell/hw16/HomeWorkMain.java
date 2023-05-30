db0d6e55d1e31a14e2188be9bdc367d40975ef47
Mykola Syrovatka <syrovatka.m.m@gmail.com>
2023-05-30 10:36:46 +0300

    did HW-16

--- src/ua/ithilell/hw16/HomeWorkMain.java
+++ src/ua/ithilell/hw16/HomeWorkMain.java
@@ -1,11 +1,72 @@
 package ua.ithilell.hw16;
 
+import java.util.Arrays;
+
 public class HomeWorkMain {
 
     public static void main(String[] args) {
-        int[] array = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        System.out.println(binarySearch(array, 2));
+        int[] array = new int[]{11, 5, 4, 2, 7, 1, 10, 3, 9, 6, 8, 10};
+        quickSort(array, 0, array.length-1);
+        //shakerSort(array);
+        System.out.println(Arrays.toString(array));
     }
+
+    public static void quickSort(int[] array, int left, int right) {
+
+        if (array.length == 0 && array == null)
+            return;
+        if (left >= right)
+            return;
+        int pivot = array[right];
+        int i = left;
+        int j = right;
+        while (i <= j) {
+            while (array[i] < pivot) {
+                i++;
+            }
+            while (array[j] > pivot) {
+                j--;
+            }
+            if (i <= j) {
+                swap(array, i, j);
+                i++;
+                j--;
+            }
+        }
+        if (left < j)
+            quickSort(array, left, j);
+
+        if (right > i)
+            quickSort(array, i, right);
+    }
+
+
+    public static void shakerSort(int[] array) {
+        int left = 0;
+        int right = array.length - 1;
+        while (left < right) {
+            for (int i = left; i < right; i++) {
+                if (array[i] > array[i + 1]) {
+                    swap(array, i, i + 1);
+                }
+            }
+            right--;
+            for (int i = right; i > left; i--) {
+                if (array[i] < array[i - 1]) {
+                    swap(array, i, i - 1);
+                }
+            }
+            left++;
+        }
+
+    }
+
+    private static void swap(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
 
 
     public static int binarySearch(int[] array, int searchInt){
